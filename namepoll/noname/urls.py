83dcf4b7c33fe6ec72638f1d688fbdd8196b4473
Feth AREZKI <feth@tuttu.info>
2011-03-07 16:41:50 +0100

    following evaluate view deletion, suppress its url

--- namepoll/noname/urls.py
+++ namepoll/noname/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('noname.views',
     url(r'^$', 'index', name='index'),
     url(r'^detail/(?P<pk>[^/]+)/$', 'detail', name='detail'),
-    url(r'^evaluate/(?P<pk>[^/]+)/$', 'evaluate', name='evaluate'),
     url(r'^next/$', 'next', name='random-new'),
     url(r'^thankyou/$', 'thankyou', name='thankyou'),
 )
