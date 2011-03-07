3b6606dce0801e63b29bf4aa8946ad4f51cdf9da
Julien Miotte <mike.perdide@gmail.com>
2011-03-07 14:11:53 +0100

    Temporary dirty trick to display forms correctly.

--- namepoll/noname/forms.py
+++ namepoll/noname/forms.py
@@ -22,8 +22,58 @@ class EvaluationForm(ModelForm):
             'value': RadioSelect(attrs={'class':'star'}, renderer=MyRFRenderer)
             }
 
+    def custom_display(self):
+        # XXX: This is a dirty method to display the form as we would want.
+        # It should be changed anytime soon.
+        return mark_safe(u"""
+<p>
+    <label for="id_value_0">What do you think this name would be to our company?</label>
+<div id=blah>
+    <label for="id_value_0">
+        <input value="0" type="radio" class="star" name="value" id="id_value_0" />
+        Prejudiciable
+    </label>
+    <label for="id_value_1">
+        <input value="1" type="radio" class="star" name="value" id="id_value_1" />
+        Valid
+    </label>
+    <label for="id_value_2">
+        <input value="2" type="radio" class="star" name="value" id="id_value_2" />
+        Great
+    </label>
+</div>
+</p>
+
+<p>
+    <label for="id_message">Message:</label>
+</p>
+<p>
+    <textarea id="id_message" rows="10" cols="40" name="message"></textarea>
+</p>""")
+
+
 class VoterForm(ModelForm):
     class Meta(object):
         model = Voter
         exclude = ('pages_seen', 'pages_voted')
 
+    def custom_display(self):
+        # XXX: This is a dirty method to display the form as we would want.
+        # It should be changed anytime soon.
+        return mark_safe(u"""
+<p>
+    <label for="id_optional_nickname">Optional nickname:</label>
+    <input id="id_optional_nickname" type="text" name="optional_nickname" maxlength="100" />
+</p>
+
+<p>
+    <label for="id_optional_email">Optional email:</label>
+    <input id="id_optional_email" type="text" name="optional_email" maxlength="100" />
+</p>
+<p>
+    <label for="id_optional_info">Optional info:</label>
+</p>
+<p>
+    <textarea id="id_optional_info" rows="10" cols="40" name="optional_info"></textarea>
+</p>
+""")
