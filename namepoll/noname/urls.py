2304cc5374b00eca28f9e26224d82d52dde83765
Feth AREZKI <feth@tuttu.info>
2011-03-07 15:28:57 +0100

    using urls

--- namepoll/noname/urls.py
+++ namepoll/noname/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import patterns, url
 
 #TODO: use direct_to_template when app is ajaxified
 
 urlpatterns = patterns('noname.views',
-    (r'^$', 'index'),
-    (r'^detail/(?P<pk>[^/]+)/$', 'detail'),
-    (r'^evaluate/(?P<pk>[^/]+)/$', 'evaluate'),
-    (r'^next/$', 'next'),
-    (r'^thankyou/$', 'thankyou'),
+    url(r'^$', 'index', name='index'),
+    url(r'^detail/(?P<pk>[^/]+)/$', 'detail', name='detail'),
+    url(r'^evaluate/(?P<pk>[^/]+)/$', 'evaluate', name='evaluate'),
+    url(r'^next/$', 'next', name='random-new'),
+    url(r'^thankyou/$', 'thankyou', name='thankyou'),
 )
