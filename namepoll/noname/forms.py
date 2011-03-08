e0e794abe57cb8128e885b27ec0c4c6a0f8b3ed9
Feth Arezki <feth@tuttu.info>
2011-03-08 17:12:38 +0100

    mark strings translatable.
    Warning: the messages maker believes
    -#-*-coding:utf-8-*-
    is about "utf-8-", not utf-8

--- namepoll/noname/forms.py
+++ namepoll/noname/forms.py
@@ -1,4 +1,4 @@
-#-*-coding:utf-8-*-
+#coding: utf-8
 from django.forms import ModelForm, RadioSelect
 from django.forms.widgets import RadioFieldRenderer
 from django.utils.safestring import mark_safe
